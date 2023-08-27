46cf176e478b06ec22d490f7d6511156e58f3fcd
JohannesBreitenbach <johannes.a.breitenbach@gmail.com>
2023-08-27 11:12:55 +0200

    added counter and email sub (visual)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-console.log("Hello")
+import { CountUp } from './countUp.min.js';
+
+//Active Navbar
 const activePage = window.location.pathname;
 const navLinks = Array.from(document.querySelectorAll('nav a')).forEach(link => {
-    if(link.href.includes(`${activePage}`)){
+    if (link.href.includes(`${activePage}`)) {
         link.classList.add('nav-selected')
         console.log("Hello")
     }
@@ -11,16 +13,22 @@ function setCountdownValue() {
     let nextCamp = new Date("2024-07-26T00:00:00");
     let now = new Date();
     let difference = nextCamp - now;
-  
+
     // Calculating time difference in days, hours, minutes, and seconds
     let days = Math.floor(difference / (1000 * 60 * 60 * 24));
     let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-  
+
     // Displaying the countdown
-    document.getElementById("countdown").innerHTML = days;
-  }
-  
-  // Call the function immediately
+    //document.getElementById("countdown").innerHTML = days;
+    const countUp = new CountUp('countdown', days, { enableScrollSpy: true });
+    if (!countUp.error) {
+        countUp.start();
+    } else {
+        console.error(countUp.error);
+    }
+}
+
+// Call the function immediately
 setCountdownValue();
